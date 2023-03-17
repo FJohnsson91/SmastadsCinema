c89dd1e9d0f8204ee674ff9e57eff38e15830e36
Fredrik Johnsson <fredrik.johnsson0029@stud.hkr.se>
2023-03-17 21:02:33 +0100

    Added React Router and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from './components/NavBar'
+import StartPage from './pages/StartPage'
+import Movies from './pages/Movies'
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+export default function App() {
+  return <>
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <Routes>
+        <Route path='/' element={<StartPage />}></Route>
+        <Route path='/movies' element={<Movies />}></Route>
+      </Routes>
+    </main>
+  </>
 }
-
-export default App
